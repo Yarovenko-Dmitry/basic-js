a06dbe4500551ca40c4ab54fb311a6886712f325
prostiak2019 <yarovenkodmv@gmail.com>
2020-03-22 22:41:32 +0300

    feat: add task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+  let result = [];
+  let firstCharacter = '';
+
+  if ( !members)  {
+    return false;
+  } else {
+    for ( let i = 0; i < members.length; i++) {
+      if( typeof members[i] == 'string') {
+        firstCharacter = members[i].trim().charAt(0).toUpperCase();
+        result.push(firstCharacter);
+      }
+    }
+    return result.sort().join('');
+  } 
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,26 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  const errorString = 'Unable to determine the time of year!';
+  
+  if ( date == undefined ) {
+    return errorString;
+  } 
+
+  if ( !date.getUTCMilliseconds() ) { throw new Error; }
+
+  let checkedMonth = date.getMonth() + 1;
+
+  if ( checkedMonth == 12 || checkedMonth == 1 || checkedMonth == 2 ) {
+    return 'winter'; 
+  }
+
+  if ( checkedMonth == 3 || checkedMonth == 4 || checkedMonth == 5 ) {
+    return 'spring'; 
+  }
+
+  if ( checkedMonth == 6 || checkedMonth == 7 || checkedMonth == 8 ) { 
+    return 'summer'; 
+  }
+
+  return 'autumn'; 
 };
